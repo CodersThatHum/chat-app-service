74762c5ee055c9face3728ea89258a2d7fb915fa
reyhanmichiels <103521934+reyhanmichiels@users.noreply.github.com>
2025-03-23 18:35:41 +0700

    feat: Add application configuration and logging interceptor (#3)

---
+++ src/main/java/com/codersthathum/chat_app_service/config/WebConfig.java
@@ -0,0 +1,15 @@
+package com.codersthathum.chat_app_service.config;
+
+import com.codersthathum.chat_app_service.interceptor.LoggerInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LoggerInterceptor()).addPathPatterns("/**");
+    }
+}
