f26b2311667068ecf9b3c1c1705e38335f349c87
reyhanmichiels <103521934+reyhanmichiels@users.noreply.github.com>
2025-03-23 19:17:19 +0700

    [FEAT] Implement rate limit interceptor (#4)

--- src/main/java/com/codersthathum/chat_app_service/config/WebConfig.java
+++ src/main/java/com/codersthathum/chat_app_service/config/WebConfig.java
@@ -1,15 +1,23 @@
 package com.codersthathum.chat_app_service.config;
 
 import com.codersthathum.chat_app_service.interceptor.LoggerInterceptor;
+import com.codersthathum.chat_app_service.interceptor.RateLimitInterceptor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@RequiredArgsConstructor
 public class WebConfig implements WebMvcConfigurer {
+
+    private final RateLimitInterceptor rateLimitInterceptor;
+
+    private final LoggerInterceptor loggerInterceptor;
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(new LoggerInterceptor()).addPathPatterns("/**");
+        registry.addInterceptor(loggerInterceptor).addPathPatterns("/**");
+        registry.addInterceptor(rateLimitInterceptor).addPathPatterns("/**");
     }
 }
