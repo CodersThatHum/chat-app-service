fdf11c69188687465b7bea542cdc95b7dbc450d8
reyhanmichiels <103521934+reyhanmichiels@users.noreply.github.com>
2025-03-23 20:15:52 +0700

    [FEAT] Enhance error handling and response structure with request context (#5)

--- src/main/java/com/codersthathum/chat_app_service/controller/AppExceptionController.java
+++ src/main/java/com/codersthathum/chat_app_service/controller/AppExceptionController.java
@@ -2,6 +2,7 @@ package com.codersthathum.chat_app_service.controller;
 
 import com.codersthathum.chat_app_service.dto.http.HttpResponse;
 import com.codersthathum.chat_app_service.exception.*;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -11,38 +12,38 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 public class AppExceptionController {
 
     @ExceptionHandler(DuplicateResourceException.class)
-    public ResponseEntity<HttpResponse<Void>> handleDuplicateResourceException(DuplicateResourceException ex) {
+    public ResponseEntity<HttpResponse<Void>> handleDuplicateResourceException(DuplicateResourceException ex, HttpServletRequest request) {
         return ResponseEntity
                 .status(HttpStatus.CONFLICT)
-                .body(HttpResponse.error(HttpStatus.CONFLICT, ex.getMessage()));
+                .body(HttpResponse.error(request, HttpStatus.CONFLICT, ex.getMessage()));
     }
 
     @ExceptionHandler(ResourceNotFoundException.class)
-    public ResponseEntity<HttpResponse<Void>> handleResourceNotFoundException(ResourceNotFoundException ex) {
+    public ResponseEntity<HttpResponse<Void>> handleResourceNotFoundException(ResourceNotFoundException ex, HttpServletRequest request) {
         return ResponseEntity
                 .status(HttpStatus.NOT_FOUND)
-                .body(HttpResponse.error(HttpStatus.NOT_FOUND, ex.getMessage()));
+                .body(HttpResponse.error(request, HttpStatus.NOT_FOUND, ex.getMessage()));
     }
 
     @ExceptionHandler(UnauthorizedException.class)
-    public ResponseEntity<HttpResponse<Void>> handleUnauthorizedException(UnauthorizedException ex) {
+    public ResponseEntity<HttpResponse<Void>> handleUnauthorizedException(UnauthorizedException ex, HttpServletRequest request) {
         return ResponseEntity
                 .status(HttpStatus.UNAUTHORIZED)
-                .body(HttpResponse.error(HttpStatus.UNAUTHORIZED, ex.getMessage()));
+                .body(HttpResponse.error(request, HttpStatus.UNAUTHORIZED, ex.getMessage()));
     }
 
     @ExceptionHandler(ForbiddenException.class)
-    public ResponseEntity<HttpResponse<Void>> handleForbiddenException(ForbiddenException ex) {
+    public ResponseEntity<HttpResponse<Void>> handleForbiddenException(ForbiddenException ex, HttpServletRequest request) {
         return ResponseEntity
                 .status(HttpStatus.FORBIDDEN)
-                .body(HttpResponse.error(HttpStatus.FORBIDDEN, ex.getMessage()));
+                .body(HttpResponse.error(request, HttpStatus.FORBIDDEN, ex.getMessage()));
     }
 
     @ExceptionHandler(TooManyRequestException.class)
-    public ResponseEntity<HttpResponse<Void>> handleTooManyRequestException(TooManyRequestException ex) {
+    public ResponseEntity<HttpResponse<Void>> handleTooManyRequestException(TooManyRequestException ex, HttpServletRequest request) {
         return ResponseEntity
                 .status(HttpStatus.TOO_MANY_REQUESTS)
-                .body(HttpResponse.error(HttpStatus.TOO_MANY_REQUESTS, ex.getMessage()));
+                .body(HttpResponse.error(request, HttpStatus.TOO_MANY_REQUESTS, ex.getMessage()));
     }
 
 }
